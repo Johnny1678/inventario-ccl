22d0d80dde880246659a89c634d29d78c7e7a932
Johnny <Johnny1678m@gmail.com>
2026-06-13 10:44:22 -0500

    Configuración de Entity Framework con PostgresSQL

---
+++ InventarioCCL.Api/Data/AppDbContext.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using InventarioCCL.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace InventarioCCL.Api.Data;
+
+public partial class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Producto> Productos { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Producto>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("productos_pkey");
+
+            entity.ToTable("productos");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Cantidad)
+                .HasDefaultValue(0)
+                .HasColumnName("cantidad");
+            entity.Property(e => e.Nombre)
+                .HasMaxLength(100)
+                .HasColumnName("nombre");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ InventarioCCL.Api/Models/Producto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InventarioCCL.Api.Models;
+
+public partial class Producto
+{
+    public int Id { get; set; }
+
+    public string Nombre { get; set; } = null!;
+
+    public int Cantidad { get; set; }
+}
